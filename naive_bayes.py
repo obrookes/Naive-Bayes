cd6c76970b69778361d458caf8cf81b6f771af1d
Jack Hanslope <code@jackhanslope.com>
2020-10-13 14:11:18 +0100

    Test test function

--- naive_bayes.py
+++ naive_bayes.py
@@ -82,7 +82,18 @@ def run():
     labelled_trump = [(s, cls[1]) for s in trump_sentences]
     labelled_data = labelled_obama + labelled_trump
     
-    return Data(labelled_data, cls)
+    foo = Data(labelled_data, cls)
+
+    trump_test = ['We', ',', 'the', 'citizens', 'of', 'America', ',', 'are', 'now', 'joined', 'in', 'a', 'great', 'national', 'effort', 'to', 'rebuild', 'our', 'country', 'and', 'restore', 'its', 'promise', 'for', 'all', 'of', 'our', 'people', '.']
+
+    obama_test = ['I', 'stand', 'here', 'today', 'humbled', 'by', 'the', 'task', 'before', 'us', ',', 'grateful', 'for', 'the', 'trust', 'you', 'have', 'bestowed', ',', 'mindful', 'of', 'the', 'sacrifices', 'borne', 'by', 'our', 'ancestors', '.']
+
+    print("trump test")
+    print(foo.test_doc(trump_test) )
+    
+    print("obama test")
+    print(foo.test_doc(obama_test) )
+
 
 if __name__ == '__main__':
     run()
